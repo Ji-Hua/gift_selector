88a2855fdfa5391221285dac60b9cbf6e518bf78
Ji Hua <michaelhua1993@foxmail.com>
2021-02-07 22:05:04 -0500

    add db migrations

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 import os
-from flask_script import Manager, shell
+from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 from app import create_app, db
@@ -12,9 +12,10 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Role': Role}
+    return {'app':app, 'db': db, 'User': User, 'Role': Role}
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
     manager.run()
